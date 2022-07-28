b794a3e618fb6001e2584737b23d638697826817
hy <18917508893@163.com>
2022-07-28 18:42:02 +0800

    feat: 响应式

--- packages/reactivity/dist/reactivity.global.js
+++ packages/reactivity/dist/reactivity.global.js
@@ -1,5 +1,25 @@
 var VueReactivity = (() => {
   // packages/reactivity/src/index.ts
   console.log("reactivity");
+  var price = 10;
+  var quantity = 2;
+  var total = 0;
+  var dep = /* @__PURE__ */ new Set();
+  var effect = () => {
+    total = price * quantity;
+  };
+  var track = () => {
+    dep.add(effect);
+  };
+  var trigger = () => {
+    dep.forEach((effect2) => effect2());
+  };
+  track();
+  console.log(`total1: ${total}`);
+  trigger();
+  console.log(`total2: ${total}`);
+  price = 20;
+  trigger();
+  console.log(`total3: ${total}`);
 })();
 //# sourceMappingURL=reactivity.global.js.map
--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,1 +1,24 @@
 console.log('reactivity')
+// 单个属性的简易版本
+let price = 10, quantity = 2, total = 0;
+const dep = new Set(); //存储需要执行的副作用（处理逻辑函数）
+const effect = ()=> {
+  total = price * quantity
+}
+// track 用于收集副作用
+const track = ()=> {
+  dep.add(effect)
+}
+// trigger 用于触发 执行副作用
+const trigger = () => {
+  dep.forEach(effect => effect() )
+}
+
+track()
+console.log(`total1: ${total}`)
+trigger()
+console.log(`total2: ${total}`)
+price = 20
+trigger()
+console.log(`total3: ${total}`)
+
